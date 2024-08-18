08ba1574c39cdb476c1e7a5942aba3b196a7cc46
jbowen999 <julian.e.bowen@outlook.com>
2024-08-17 20:58:54 -0700

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,34 @@
+﻿namespace CSC205Final
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // Testing
+            Console.WriteLine(IsDoubloon("Noon"));       // True
+            Console.WriteLine(IsDoubloon("Civic"));      // False
+            Console.WriteLine(IsDoubloon("Moon"));       // False
+            Console.WriteLine(IsDoubloon("Rotor"));      // False
+            Console.WriteLine(IsDoubloon("Deeded"));     // True
+            Console.WriteLine(IsDoubloon("Totem"));      // False
+            Console.WriteLine(IsDoubloon("Night"));      // False
+        }
+
+        public static bool IsDoubloon(string word)
+        {
+            
+            word = word.ToLower();
+
+            
+            foreach (char c in word)
+            {
+                if (word.Count(ch => ch == c) != 2)
+                {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+    }
+}
